77459f7b552cc027c067789dd0a9b7fece14078f
Manuelfdzslz <102754983+Manuelfdzslz@users.noreply.github.com>
2022-04-28 14:35:41 -0500

    fluent validation on login model

--- TokaApi/TokaApi/Models/LogIn.cs
+++ TokaApi/TokaApi/Models/LogIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,5 +10,15 @@ namespace TokaApi.Models
     {
         public string Email { get; set; }
         public string Password { get; set; }
+    }
+
+
+    public class LogInValidator : AbstractValidator<LogIn>
+    {
+        public LogInValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+        }
     }
 }
